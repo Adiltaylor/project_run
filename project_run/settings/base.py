41f36b33dc6c20a9e89dab8c3cd8a31b78eb90c1
PC <adiltaylor@gmail.com>
2026-06-03 13:03:39 +0500

    Установил djangorestframework

--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -114,3 +115,7 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPANY_NAME = 'Ozinmen kures'
+SLOGAN = 'Deni saudyn jany sau'
+CONTACTS = '+7 707 582 2298'

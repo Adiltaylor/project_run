802ed0bd51f85b00a77b3d67569e1ad9e9f22cac
PC <adiltaylor@gmail.com>
2026-06-03 22:47:51 +0500

    Задание 2. создания model Run 2 часть

--- app_run/views.py
+++ app_run/views.py
@@ -1,6 +1,10 @@
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
+
+from app_run.models import Run
+from app_run.serializers import RunSerializer
 
 
 @api_view(['GET'])
@@ -9,3 +13,6 @@ def company_details(request):
                'slogan': settings.SLOGAN,
                'contacts': settings.CONTACTS}
     return Response(details)
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer

2e9a5c07903bfece2e1266cdb1034a0502b50eaf
PC <adiltaylor@gmail.com>
2026-06-10 10:41:25 +0500

    Задание 3. Атлеты и тренеры создания model Users

--- app_run/views.py
+++ app_run/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
 
 from app_run.models import Run
-from app_run.serializers import RunSerializer
+from app_run.serializers import RunSerializer, UserSerializer
 
 
 @api_view(['GET'])
@@ -13,6 +14,21 @@ def company_details(request):
                'slogan': settings.SLOGAN,
                'contacts': settings.CONTACTS}
     return Response(details)
+
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.filter(is_superuser=False)
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        qs = self.queryset
+        is_staff = self.request.query_params.get('is_staff', None)
+        if is_staff:
+            qs = qs.filter(is_staff=is_staff)
+        return qs
+
+
+

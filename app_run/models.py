0101fb8572fe159535b99199ba45a30d41dbfee5
PC <adiltaylor@gmail.com>
2026-06-11 11:20:18 +0500

    Задание 3. Больше данных о атлетах 2

--- app_run/models.py
+++ app_run/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 class Run(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
-    athlete = models.ForeignKey(User, on_delete=models.CASCADE,related_name='runs_as_athlete')
+    #athlete = models.ForeignKey(User, on_delete=models.CASCADE,related_name='runs_as_athlete')
 
 

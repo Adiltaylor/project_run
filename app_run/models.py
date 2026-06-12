30da42fa8cf901c7e40faeea41b3c1cab302d161
PC <adiltaylor@gmail.com>
2026-06-12 21:40:10 +0500

    Задание 5. Поиск юзера witout comment2

--- app_run/models.py
+++ app_run/models.py
@@ -5,5 +5,4 @@ class Run(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
     athlete = models.ForeignKey(User, on_delete=models.CASCADE,related_name='runs_as_athlete')
-    comment1 = models.TextField()
 
